32eaa956a19e9b8d72c79b5fb091b6978ecfcbda
AlexT <alex.treid@gmail.com>
2025-03-18 21:16:25 +0300

    lab_1_2_final

--- Lab1.2/data_temp.py
+++ Lab1.2/data_temp.py
@@ -1,10 +1,9 @@
+#!/usr/local/bin/python3
 from openpyxl import load_workbook
 from matplotlib import pyplot
 
-
-wb = load_workbook('/Users/alextreid/Downloads/data_analysis_lab.xlsx')
+wb = load_workbook('data_analysis_lab.xlsx')
 sheet = wb['Data']
-sheet['A']
 
 def getvalue(x): return x.value
 
@@ -12,8 +11,11 @@ list_x = list(map(getvalue, sheet['A'][1:]))
 list_y = list(map(getvalue, sheet['C'][1:]))
 list_z = list(map(getvalue, sheet['D'][1:]))
 
-import matplotlib
-matplotlib.use('TkAgg')
 pyplot.plot(list_x, list_y, label="Tempreature")
-pyplot.show
+pyplot.plot(list_x, list_z, label="Sun Activity")
 
+pyplot.xlabel('Years')
+pyplot.ylabel('Tempreature')
+pyplot.suptitle('Sun Activity and Tempreature')
+
+pyplot.show()
